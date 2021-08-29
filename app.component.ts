2780381e3b2c2a0b62900fbf9e1bb7778a9ae586
maihelmy <88769111+Maihelmi@users.noreply.github.com>
2021-08-29 13:03:47 +0200

    Add files via upload

--- app.component.ts
+++ app.component.ts
@@ -39,4 +39,21 @@ export class AppComponent {
  toggle(){
    return this.isheader=!this.isheader
  }
+  PRODUCTS = [
+  {"id":1,"name":"Licensed Frozen Hat","description":"Incidunt et magni","price":"170.00","quantity":56840},
+  {"id":2,"name":"Rustic Concrete Chicken","description":"Sint libero mollitia","price":"302.00","quantity":9358},
+  {"id":3,"name":"Fantastic Metal Computer","description":"In consequuntur cupiditat","price":"279.00","quantity":90316},
+  {"id":4,"name":"Refined Concrete Chair","description":"Saepe nemo praesentium","price":"760.00","quantity":5899}
+];
+ num:number=3
+day(){
+  return this.num
+}
+
+
+
+
+
+
+
 }
